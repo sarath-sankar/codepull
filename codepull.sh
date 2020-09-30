f144e1e291a813b46e5ce7c1b169d716430304f5
Ubuntu <sarath@whitesource-test.oq5cr0xvhyeu5mjmdlzt4lkl2d.bx.internal.cloudapp.net>
2020-09-30 11:56:57 +0000

    adding execute code

---
+++ codepull.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+####Downloading files #####
+set -a
+echo -e "Dowinloading script to pwd ....\n"
+
+wget https://raw.githubusercontent.com/sarath-sankar/codepull/master/docker.sh -O $PWD/docker.sh
+wget https://raw.githubusercontent.com/sarath-sankar/codepull/master/pullappz.sh -O $PWD/pullappz.sh
+wget https://raw.githubusercontent.com/sarath-sankar/codepull/master/setalias.sh -O $PWD/setalias.sh
+
+##### installing #######
+echo -e "Runing script ..........."
+if [[ $? -eq 0 ]];then
+	chmod u+x $PWD/docker.sh
+	chmod u+x $PWD/pullappz.sh
+	chmod u+x $PWD/setalias.sh
+	bash $PWD/docker.sh &&  bash $PWD/pullappz.sh $$ echo "Now setting alias ..."
+	cmd="alias setal='$PWD/setalias.sh && source $HOME/.bashrc '"
+	#echo $cmd
+        grep -qF -- "$cmd" $HOME/.bashrc || echo $cmd >> $HOME/.bashrc
+        source $HOME/.bashrc 
+	set +a
+fi
+
+
+rm $PWD/docker.sh 
+rm $PWD/pullappz.sh
+echo -e "hope it works "
